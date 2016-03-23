67b26c100e933e938a10db4d4c25e78011a4ae65
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-03-23 13:47:52 +0000

    Fix problem while accessing a pipeline where commentables list is like [nil]

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 #This file is part of SEQUENCESCAPE; it is distributed under the terms of GNU General Public License version 1 or later;
 #Please refer to the LICENSE and README files for information on licensing and authorship of this file.
 #Copyright (C) 2007-2011,2015,2016 Genome Research Ltd.
-
 class Comment < ActiveRecord::Base
   # include Uuid::Uuidable
   belongs_to :commentable, :polymorphic => true
@@ -36,7 +35,7 @@ class Comment < ActiveRecord::Base
   scope :include_uuid, -> { where('TRUE') }
 
   def self.counts_for(commentables)
-    return 0 if commentables.empty?
+    return {} if commentables.compact.empty?
     type = commentables.first.class.base_class.name
     where(:commentable_type=>type,:commentable_id=>commentables).group(:commentable_id).count
   end
