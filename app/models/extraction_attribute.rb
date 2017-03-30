31c6a0e2bb00f14f708213e5279ae5322af528b2
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-03-31 00:11:17 +0100

    Refactor of extraction attribute

--- app/models/extraction_attribute.rb
+++ app/models/extraction_attribute.rb
@@ -14,38 +14,50 @@ class ExtractionAttribute < ActiveRecord::Base
 
   before_save :update_performed
 
+  def is_reracking_attribute?(well)
+    !well['previous_plate_uuid'].nil?
+  end
+
   def update_performed
     if attributes_update['wells']
-      location_wells = target.wells.includes(:map, :sample).index_by(&:map_description)
       attributes_update['wells'].each do |w|
-        location = w['location']
-        next unless w['sample_tube_uuid']
-        sample_tube = Uuid.find_by(external_id: w['sample_tube_uuid']).resource
-        aliquots = sample_tube.aliquots.map(&:dup)
-        samples = sample_tube.samples
-        destination_well = location_wells[location]
-
-        unless destination_well.samples.include?(samples)
-          destination_well.aliquots << aliquots
-          AssetLink.create_edge(sample_tube, destination_well)
+        if is_reracking_attribute?(w)
+          rerack_well(w)
+        else
+          rack_well(w)
         end
       end
     end
-    if attributes_update['reracks']
-      rerack_updates
-    end
   end
 
-  def rerack_updates
-    attributes_update['reracks'].each do |t|
-      previous_parent = Uuid.find_by(external_id: t['previous_plate_uuid']).resource
-      well = previous_parent.well_at(t['previous_location'])
-      actual_parent = Uuid.find_by(external_id: t['actual_plate_uuid']).resource
-      actual_parent.well_at(t['actual_location']).destroy
+  def location_wells
+    target.wells.includes(:map, :sample).index_by(&:map_description)
+  end
+
+  def rack_well(well_data)
+    return unless well_data['sample_tube_uuid']
+    sample_tube = Uuid.find_by(external_id: well_data['sample_tube_uuid']).resource
+    aliquots = sample_tube.aliquots.map(&:dup)
+    samples = sample_tube.samples
+    location = well_data['location']
+    destination_well = location_wells[location]
+
+    unless destination_well.samples.include?(samples)
+      destination_well.aliquots << aliquots
+      AssetLink.create_edge(sample_tube, destination_well)
+    end    
+  end
+
+  def rerack_well(well_data)
+    previous_parent = Uuid.find_by(external_id: well_data['previous_plate_uuid']).resource
+    well = previous_parent.well_at(well_data['previous_location'])
+    unless well.aliquots.empty?
+      actual_parent = Uuid.find_by(external_id: well_data['actual_plate_uuid']).resource
+      actual_parent.well_at(well_data['actual_location']).destroy
 
       well.update_attributes(parent: actual_parent)
       actual_parent.wells << well
-    end
+    end    
   end
 
   private :update_performed
